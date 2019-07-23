f3846ac806aa119d7cdc0882746184aa8ccc9643
Saikiran <sai.kiran@mastek.com>
2019-07-23 11:57:46 +0530

    1 change

--- src/main/java/com/supermarket/controller/AdminController.java
+++ src/main/java/com/supermarket/controller/AdminController.java
@@ -7,18 +7,21 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.supermarket.model.Admin;
 import com.supermarket.repository.AdminRepository;
 
 
-
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
 public class AdminController {
 	@Autowired
 	AdminRepository repository;
